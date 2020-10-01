874a43db984b15068cc5de75b2807cd25d7eb825
Jonah D. Hadlock <jonah-hadlock@lambdastudents.com>
2020-09-30 20:50:35 -0600

    nearly finished; just need the contact and nav section

--- README.md
+++ README.md
@@ -12,28 +12,11 @@ You have been provided a [JSON object](js/index.js) with all the necessary data 
 
 ## Instructions
 
-### Task 1: Set Up The Project With Git
-
-**Follow these steps to set up and work on your project:**
-
-* [ ] Create a forked copy of this project.
-* [ ] Clone your OWN version of the repository (Not Lambda's by mistake!).
-* [ ] Create a new branch: git checkout -b `<firstName-lastName>`.
-* [ ] Implement the project on your newly created `<firstName-lastName>` branch, committing changes regularly.
-* [ ] Push commits: git push origin `<firstName-lastName>`.
-
 ### Task 2a: MVP
 
 #### Create selectors to point your data into elements
 
-* [ ] Create selectors by using any of the DOM element's methods
-* [ ] Note that IDs have been used on all images. Use the IDs to update src path content
-
 #### Update the HTML with the JSON data
-
-* [ ] Remember, NO direct updating of the HTML source is allowed.
-* [ ] Using your selectors, update the content to match the example file.
-* [ ] Remember to update the src attributes on images
 
 #### Add new content
 
@@ -41,17 +24,3 @@ You have been provided a [JSON object](js/index.js) with all the necessary data 
 * [ ] Utilize `.appendChild()` and `.prepend()` to add two new items to the navigation system. You can call them whatever you want.
 * [ ] Check your work by looking at the [original html](original.html) in the browser
 
-### Task 3: Stretch
-
-* [ ] Update styles throughout the page as you see fit. Study what happens when you updated the DOM using style in JavaScript.  
-* [ ] Study tomorrow's lesson on events and try to integrate a button that can update content on the site with a click of a button.  You could build a similar data object with new values to help you test the click event.
-
-#### Stretch Project: Digital Timer
-
-This project is heavier on logic but employs some DOM manipulation to achieve its goals.  Go check it out here: [stretch assignment](stretch-assignment) and see how far you can get.
-
-## Submission Format
-
-**Follow these steps for completing your project.**
-
-* [ ] Submit a Pull-Request to merge `<firstName-lastName>` Branch into `main` (student's  Repo). **Please don't merge your own pull request**
--- js/index.js
+++ js/index.js
@@ -1,42 +1,107 @@
 const siteContent = {
-  "nav": {
-    "nav-item-1": "Services",
-    "nav-item-2": "Product",
-    "nav-item-3": "Vision",
-    "nav-item-4": "Features",
-    "nav-item-5": "About",
-    "nav-item-6": "Contact",
-    "img-src": "img/logo.png"
-  },
-  "cta": {
-    "h1": "DOM Is Awesome",
-    "button": "Get Started",
-    "img-src": "img/header-img.png"
-  },
-  "main-content": {
-    "features-h4":"Features",
-    "features-content": "Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
-    "about-h4":"About",
-    "about-content": "About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
-    "middle-img-src": "img/mid-page-accent.jpg",
-    "services-h4":"Services",
-    "services-content": "Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
-    "product-h4":"Product",
-    "product-content": "Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
-    "vision-h4":"Vision",
-    "vision-content": "Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
-  },
-  "contact": {
-    "contact-h4" : "Contact",
-    "address" : "123 Way 456 Street Somewhere, USA",
-    "phone" : "1 (888) 888-8888",
-    "email" : "sales@greatidea.io",
-  },
-  "footer": {
-    "copyright" : "Copyright Great Idea! 2018"
-  },
+    "nav": {
+        "nav-item-1": "Services",
+        "nav-item-2": "Product",
+        "nav-item-3": "Vision",
+        "nav-item-4": "Features",
+        "nav-item-5": "About",
+        "nav-item-6": "Contact",
+        "img-src": "img/logo.png"
+    },
+    "cta": {
+        "h1": "DOM Is Awesome",
+        "button": "Get Started",
+        "img-src": "img/header-img.png"
+    },
+    "main-content": {
+        "features-h4": "Features",
+        "features-content": "Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
+        "about-h4": "About",
+        "about-content": "About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
+        "middle-img-src": "img/mid-page-accent.jpg",
+        "services-h4": "Services",
+        "services-content": "Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
+        "product-h4": "Product",
+        "product-content": "Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
+        "vision-h4": "Vision",
+        "vision-content": "Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
+    },
+    "contact": {
+        "contact-h4": "Contact",
+        "address": "123 Way 456 Street Somewhere, USA",
+        "phone": "1 (888) 888-8888",
+        "email": "sales@greatidea.io",
+    },
+    "footer": {
+        "copyright": "Copyright Great Idea! 2018"
+    },
 };
 
-// Example: Update the img src for the logo
-let logo = document.getElementById("logo-img");
-logo.setAttribute('src', siteContent["nav"]["img-src"])
+/** My Code Below */
+
+/** Structure */
+const title = document.querySelector('title');
+const script = document.querySelector('script');
+const html = document.querySelector('html');
+const body = document.querySelector('body');
+const head = document.querySelector('head');
+const header = document.querySelector('header');
+const h1 = document.querySelector('h1');
+const nav = document.querySelectorAll('nav');
+const div = document.querySelectorAll('div');
+const button = document.querySelector('button');
+const p = document.querySelectorAll('p');
+const h4 = document.querySelectorAll('h4');
+const link = document.querySelectorAll('link');
+const a = document.querySelectorAll('a');
+const section = document.querySelectorAll('section');
+const footer = document.querySelector('footer');
+
+/** Class */
+const containerClass = document.querySelector('.container');
+const logoClass = document.querySelector('.logo');
+const ctaClass = document.querySelector('.cta');
+const ctaTextClass = document.querySelector('.cta-text');
+const mainContentClass = document.querySelector('.main-content');
+const topContentClass = document.querySelector('.top-content');
+const textContentClass = document.querySelector('.text-content');
+const bottomContentClass = document.querySelector('.bottom-content');
+const contactClass = document.querySelector('.contact');
+
+/** Images/IDs */
+const img = document.querySelectorAll('img');
+const headerImg = document.querySelector('header-img');
+const logoImg = document.querySelector('logo-img');
+const ctaImg = document.querySelector('cta-img');
+const middleImg = document.querySelector('middle-img');
+
+/** Updates*/
+const imgUpdate = document.querySelector("#logo-img");
+imgUpdate.src = "img/logo.png";
+const imgUpdate2 = document.querySelector('#cta-img');
+imgUpdate2.src = "img/header-img.png";
+const imgUpdate3 = document.querySelector('#middle-img');
+imgUpdate3.src = "img/mid-page-accent.jpg";
+nav.textContent = siteContent["nav"]["nav-item-1"];
+nav.textContent = siteContent["nav"]["nav-item-2"];
+nav.textContent = siteContent["nav"]["nav-item-3"];
+nav.textContent = siteContent["nav"]["nav-item-4"];
+nav.textContent = siteContent["nav"]["nav-item-5"];
+nav.textContent = siteContent["nav"]["nav-item-6"];
+button.textContent = siteContent["cta"]["button"];
+h1.textContent = siteContent["cta"]["h1"];
+h4[0].textContent = siteContent["main-content"]["features-h4"];
+p[0].textContent = siteContent["main-content"]["features-content"];
+h4[1].textContent = siteContent["main-content"]["about-h4"];
+p[1].textContent = siteContent["main-content"]["about-content"];
+h4[2].textContent = siteContent["main-content"]["services-h4"];
+p[2].textContent = siteContent["main-content"]["services-content"];
+h4[3].textContent = siteContent["main-content"]["product-h4"];
+p[3].textContent = siteContent["main-content"]["product-content"];
+h4[4].textContent = siteContent["main-content"]["vision-h4"];
+p[4].textContent = siteContent["main-content"]["vision-content"];
+contactClass.textContent = siteContent["contact"]["contact-h4"];
+contactClass.textContent = siteContent["contact"]["address"];
+contactClass.textContent = siteContent["contact"]["phone"];
+contactClass.textContent = siteContent["contact"]["email"];
+footer.textContent = siteContent["footer"]["copyright"];
